b26de202ce2d29b26a1c8a35fb139de9a379e0d4
Josue <52310147+josuedhernandez@users.noreply.github.com>
2020-09-23 07:30:26 -0700

    Updated test for List Render without crashing

--- src/List.test.js
+++ src/List.test.js
@@ -5,37 +5,37 @@ import renderer from "react-test-renderer";
 
 const cards = [
   { id: "a", title: "First card", content: "lorem ipsum" },
-  { id: "b", title: "Second card", content: "lorem ipsum" },
-  { id: "c", title: "Third card", content: "lorem ipsum" },
-  { id: "d", title: "Fourth card", content: "lorem ipsum" },
-  { id: "e", title: "Fifth card", content: "lorem ipsum" },
-  { id: "f", title: "Sixth card", content: "lorem ipsum" },
-  { id: "g", title: "Seventh card", content: "lorem ipsum" },
-  { id: "h", title: "Eighth card", content: "lorem ipsum" },
-  { id: "i", title: "Ninth card", content: "lorem ipsum" },
-  { id: "j", title: "Tenth card", content: "lorem ipsum" },
-  { id: "k", title: "Eleventh card", content: "lorem ipsum" },
-  { id: "l", title: "Twelfth card", content: "lorem ipsum" },
-  { id: "m", title: "Thirteenth card", content: "lorem ipsum" },
+  { id: "b", title: "Second card", content: "lorem ipsum" }
 ];
+
+const cardsEmpty = [];
 
 describe("List component", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<List />, div);
+    ReactDOM.render(<List key={"1"}
+             header={"First List"}
+             cards={cards}/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it("renders the UI as expected", () => {
+  it("renders the UI as expected no cards", () => {
     const tree = renderer
       .create(
         <List
           key={"1"}
           header={"First List"}
-          cards={cards}
+          cards={cardsEmpty}
         />
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+    it("renders the UI as expected with cards", () => {
+      const tree = renderer
+        .create(<List key={"1"} header={"First List"} cards={cards} />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
 });
